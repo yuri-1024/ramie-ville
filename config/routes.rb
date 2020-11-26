ca17db0ad0a5684c24a4ae844d023dc6ba859aab
yuri-1024 <yuri.zzz.o0@gmail.com>
2020-11-26 19:12:56 +0900

    [add] model作成/devise追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :creaters, controllers: {
+  	sessions: 'creaters/sessions',
+  	passwords: 'creaters/passwords',
+  	registrations: 'creaters/registrations'
+  }
+
+  devise_for :admins, controllers: {
+  	sessions:      'admins/sessions',
+  	passwords:     'admins/passwords',
+  	registrations: 'admins/registrations'
+  }
+
+  devise_for :end_users, controllers: {
+  	sessions:      'end_users/sessions',
+  	passwords:     'end_users/passwords',
+  	registrations: 'end_users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+
 end
