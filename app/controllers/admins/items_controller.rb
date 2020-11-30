6cf28e44fc3192fb512554f16c51bbfb5b50f5c8
yuri-1024 <yuri.zzz.o0@gmail.com>
2020-11-30 18:57:16 +0900

    [add] admins/item追加

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,2 +1,26 @@
 class Admins::ItemsController < ApplicationController
+
+	def index
+		@items = Item.all
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	def edit
+		@item = Item.find(params[:id])
+	end
+
+	def update
+		@item = Item.find(params[:id])
+		@item.update(item_params)
+		redirect_to admins_item_path(@item.id)
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit(:name, :body, :price, :status, :tag_list, :image_id, :creater_id)
+	end
 end
