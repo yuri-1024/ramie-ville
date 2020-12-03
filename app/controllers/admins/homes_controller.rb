c9d6d9892cbf067c95451a0cf3038a8a996a483c
yuri-1024 <yuri.zzz.o0@gmail.com>
2020-12-03 17:26:51 +0900

    [update]admins/homes

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,5 +1,9 @@
 class Admins::HomesController < ApplicationController
+
 	def index
-		
+		@contacts = Contact.where(status: "未対応")
+		@order_news = Order.where(status: "入金待ち")
+		@order_ends = Order.where(status: "発送準備中")
 	end
+
 end
