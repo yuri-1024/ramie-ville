1df762dba83cbb1d1e3772a7f85496a70e5070b0
yuri-1024 <yuri.zzz.o0@gmail.com>
2020-12-03 16:07:50 +0900

    [update] creaters/homes

--- app/controllers/creaters/homes_controller.rb
+++ app/controllers/creaters/homes_controller.rb
@@ -1,5 +1,8 @@
 class Creaters::HomesController < ApplicationController
 
 	def index
+		@order_item_news = OrderItem.where(creat_status: "未着手")
+		@order_item_goings = OrderItem.where(creat_status: "製作中")
+		@item = Item.all
 	end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
 	belongs_to :item
 	belongs_to :order
 
-	enum creat_status: { 着手不可: 0, 製作中: 1}
+	enum creat_status: { 着手不可: 0, 未着手: 1, 製作中: 2, 製作完了: 3}
 end
